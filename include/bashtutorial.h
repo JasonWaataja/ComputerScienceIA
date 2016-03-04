572af37da83710dcce43aaea1547a86d44ebbcb3
Jason Waataja <jasonswaataja@gmail.com>
2016-03-04 11:37:41 -0800

    added skeleton to BashTutorial.h to be implemented later

---
+++ include/bashtutorial.h
@@ -0,0 +1,27 @@
+#ifndef BASHTUTORIAL_H
+#define BASHTUTORIAL_H
+
+#include <vector>
+
+#include "tutorial.h"
+
+class BashTutorial
+{
+	private:
+		//list of all tutorials that are available
+		vector<Tutorial> availableTutorials;
+
+		//scans all of the tutorial files in tutorialDirectory and loads them into availableTutorials
+		void loadTutorialsFromDirectory(const char *tutorialDirectory);
+	public:
+		BashTutorial();
+		BashTutorial(const char *tutorialDirectory);
+		~BashTutorial();
+
+		//method to have the user select a tutorial from a the current list
+		Tutorial *selectTutorialMenu();
+		//method to call to run the whole tutorial start to finish
+		void startBashTutorial();
+};
+
+#endif
