91960e6e9799356cc1426227e95b6179762b685f
Jason Waataja <jasonswaataja@gmail.com>
2016-03-09 09:51:40 -0800

    Fixed file naming, added functions in header
    Renamed src/readline.h to src/readline.cpp which allowed it to build.
    Also added function prototypes to header file getBash Input() and
    getBashInputExpected().

--- include/bashcommandline.h
+++ include/bashcommandline.h
@@ -1,4 +1,15 @@
 #ifndef BASHCOMMANDLINE_H
 #define BASHCOMMANDLINE_H
 
+#include <string>
+
+#define MISTAKE_COUNT_HINT 5
+
+using namespace std;
+
+//gets the user input with a bash-like command line
+string getBashInput();
+//errors until the user enters the correct input, gives a hint after MISTAKE_COUNT_HINT incorrect guessed
+string getBashInputExpected(const string& expected, const string& incorrectMessage = "Incorrect", const string& hintMessage = "Looks lilke you need a hint.");
+
 #endif
---
+++ src/bashcommandline.cpp
@@ -0,0 +1,5 @@
+#include "bashcommandline.h"
+
+#include <readline/readline.h>
+
+
